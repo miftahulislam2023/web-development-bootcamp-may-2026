cbbe4c5e39b851bec8ceae9d799111323176e303
Samioul51 <akmsamioulislam51@gmail.com>
2026-05-10 00:48:07 +0600

    feat: initialize firebase authentication

--- a_k_m_samioul_islam/frontend/src/main.jsx
+++ a_k_m_samioul_islam/frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router'
-import { router } from './routes/route.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import { router } from "./routes/route.jsx";
+import AuthProvider from "./providers/AuthProvider/AuthProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}>
-
-  </RouterProvider>
-)
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <RouterProvider router={router}></RouterProvider>
+  </AuthProvider>
+);
---
+++ a_k_m_samioul_islam/frontend/src/providers/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+
+    const authData={
+
+    };
+
+    return <AuthContext value={authData}>{children}</AuthContext>
+};
+
+export default AuthProvider;

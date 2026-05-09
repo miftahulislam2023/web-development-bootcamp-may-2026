ce3564f257c7c088ac8eb1bbcdf3a1e5a5186762
Samioul51 <akmsamioulislam51@gmail.com>
2026-05-10 01:06:32 +0600

    feat: setup project structure and mongoose in the project

---
+++ a_k_m_samioul_islam/backend/src/config/db/dbConnect.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB=async ()=>{
+    try {
+        const connectionInstance=await mongoose.connect(process.env.MONGODB_URI)
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.log("MongoDB Connection Failed ",error);
+    }
+}
+
+export default connectDB;
---
+++ a_k_m_samioul_islam/backend/src/index.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import connectDB from "./config/db/dbConnect.js";
+import app from "./app.js";
+
+const port = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("DB connection failed", error.message);
+  });
+
+app.get("/",(req,res)=>{
+    res.send("Server running")
+});

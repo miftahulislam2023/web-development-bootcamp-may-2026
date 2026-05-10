18721b06fe6b8fd0ad14c1d911a2aa67a53223a7
Nazmul Hasan Shajib <shojib116@gmail.com>
2026-05-11 05:59:56 +0600

    feat: add in memory message persistance

--- shojib116/client/src/routes/+page.svelte
+++ shojib116/client/src/routes/+page.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
+  type Message = {
+    text: string;
+    sentAt?: string;
+  };
   let msg = $state("");
-  const messages: string[] = $state([]);
+  const messages: Message[] = $state([]);
 
   const conn = new WebSocket(import.meta.env.VITE_WS_URL);
   conn.onclose = () => {
-    messages.push("Connection closed");
+    messages.push({ text: "Connection closed" });
   };
 
   conn.onmessage = (e) => {
-    console.log(e.data);
-    messages.push(e.data);
+    const msgs = JSON.parse(e.data);
+    console.log(JSON.parse(e.data));
+    messages.push(...msgs);
   };
 
   const handleSubmit = (e: SubmitEvent) => {
@@ -21,7 +26,7 @@
 
 <div id="messages">
   {#each messages as message}
-    <p>{message}</p>
+    <p>{message.text}</p>
   {/each}
 </div>
 <form onsubmit={handleSubmit}>
--- shojib116/server/hub.go
+++ shojib116/server/hub.go
@@ -1,10 +1,22 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"time"
+)
+
+type Message struct {
+	Text   string    `json:"text"`
+	SentAt time.Time `json:"sentAt"`
+}
+
 type Hub struct {
 	clients    map[*Client]bool
 	broadcast  chan []byte
 	register   chan *Client
 	unregister chan *Client
+	messages   []Message
 }
 
 func newHub() *Hub {
@@ -13,6 +25,7 @@ func newHub() *Hub {
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
+		messages:   []Message{},
 	}
 }
 
@@ -21,15 +34,32 @@ func (h *Hub) run() {
 		select {
 		case client := <-h.register:
 			h.clients[client] = true
+			msg, err := json.Marshal(h.messages)
+			if err != nil {
+				log.Printf("Broadcasting error on new client: %v", err)
+				continue
+			}
+			client.send <- msg
 		case client := <-h.unregister:
 			if _, ok := h.clients[client]; ok {
 				delete(h.clients, client)
 				close(client.send)
 			}
 		case message := <-h.broadcast:
+			msg := Message{
+				Text:   string(message),
+				SentAt: time.Now(),
+			}
+			marshalledMsg, err := json.Marshal([]Message{msg})
+			if err != nil {
+				log.Printf("Broadcasting error: %v", err)
+				continue
+			}
+			h.messages = append(h.messages, msg)
+
 			for client := range h.clients {
 				select {
-				case client.send <- message:
+				case client.send <- marshalledMsg:
 				default:
 					close(client.send)
 					delete(h.clients, client)

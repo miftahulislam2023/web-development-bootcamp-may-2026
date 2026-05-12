ac178e8ff749399861ed1d92e7a677f3b1ff6015
taslima <garryanderson337@gmail.com>
2026-05-13 01:04:45 +0600

    all message routes are added

--- LifeLogChitchat/chitchat-server/src/routes/message.route.js
+++ LifeLogChitchat/chitchat-server/src/routes/message.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllContacts, getMessagesByUserId, sendMessage } from "../controllers/message.controller.js";
+import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from "../controllers/message.controller.js";
 import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
 
@@ -11,9 +11,10 @@ const router = express.Router();
 
 router.use(arcjetProtection, protectRoute);
 
-router.get("/contacts",protectRoute, getAllContacts);
-// router.get("/chats", getChatPartners);
-router.get("/:id",protectRoute, getMessagesByUserId);
-router.post("/send/:id",protectRoute, sendMessage);
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
+
 
 export default router;

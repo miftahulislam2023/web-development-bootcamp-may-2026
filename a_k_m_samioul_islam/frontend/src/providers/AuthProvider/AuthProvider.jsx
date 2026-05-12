9f243024b5ff0e46689c748b2c9c7426644a1261
Samioul51 <akmsamioulislam51@gmail.com>
2026-05-13 00:45:17 +0600

    feat(authentication): add login, register, logout functions in auth context

--- a_k_m_samioul_islam/frontend/src/providers/AuthProvider/AuthProvider.jsx
+++ a_k_m_samioul_islam/frontend/src/providers/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,39 @@
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../../firebase/firebase.init.js";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser]=useState(null);
+    const [loading, setLoading]=useState(true);
+    const [token, setToken] = useState(null);
+    const [userData, setUserData] = useState(null);
+
+    // Register
+
+    const register= (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    // Login
+
+    const login= (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    // Logout
+
+    const logout = () => {
+        return signOut(auth);
+    };
+
+    
 
     const authData={
-
+        register,
+        login,
+        logout
     };
 
     return <AuthContext value={authData}>{children}</AuthContext>

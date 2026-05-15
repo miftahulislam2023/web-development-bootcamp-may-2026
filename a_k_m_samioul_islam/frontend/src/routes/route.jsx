6e5e1799e9e079f81385a654f76eafe0692c6215
Samioul51 <akmsamioulislam51@gmail.com>
2026-05-16 05:20:14 +0600

    fix: address path related issue

--- a_k_m_samioul_islam/frontend/src/routes/route.jsx
+++ a_k_m_samioul_islam/frontend/src/routes/route.jsx
@@ -4,7 +4,7 @@ import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import Register from "../pages/register/Register";
 import NotFound from "../pages/notfound/NotFound";
-import PublicRoute from "../providers/publicRoute/PublicRoute";
+import PublicRoute from "../providers/PublicRoute/PublicRoute";
 import DashboardLayout from "../layouts/DashboardLayout/DashboardLayout";
 import MyActivity from "../pages/my-activity/MyActivity";
 import PrivateRoute from "../providers/PrivateRoute/PrivateRoute";

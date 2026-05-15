84c6bcdc9fea240511569e34dd7eb2906879a9cb
Jakaria Ahmed <jakaria88520@gmail.com>
2026-05-15 20:34:06 +0600

    remove unnecessary assets

--- jakaria2055/chat_app/src/assets/assets.js
+++ jakaria2055/chat_app/src/assets/assets.js
@@ -2,7 +2,6 @@ import avatar_icon from './avatar_icon.png'
 import logo_icon from './logo_icon.svg'
 import logo_big from './logo_big.svg'
 import logo from './logo.png'
-import profile_martin from './profile_martin.png'
 import send_button from './send_button.svg'
 import code from './code.svg'
 import bgImage from './bgImage.svg'
@@ -16,7 +15,6 @@ const assets = {
     send_button,
     code,
     bgImage,
-    profile_martin
 }
 
 export default assets;

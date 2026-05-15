5c994ea9a2744ed9219b99a8123d933699d1e828
prantonub <prantonub.cse23@gmail.com>
2026-05-15 22:55:14 +0600

    update server url

--- prantonub/client/vite.config.js
+++ prantonub/client/vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-  server: { port:5173, proxy:{ "/api":{ target:"http://localhost:5000", changeOrigin:true } } },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://financehub-personal-expence-tracker.onrender.com",
+        changeOrigin: true,
+      },
+    },
+  },
 });

ccf6b733c9613057a2a070455b4f2517a4fda277
AbuShahma022 <abushahma1014@gmail.com>
2026-05-16 12:04:20 +0600

    frontend proxy bux fix and dashboard edit

--- AbuShahma022_expence_tracker/frontend/expansetracker/app/dashboard/layout.tsx
+++ AbuShahma022_expence_tracker/frontend/expansetracker/app/dashboard/layout.tsx
@@ -1,12 +1,60 @@
+"use client"
+
+import { useEffect } from "react"
+
+import { useRouter } from "next/navigation"
+
 import Sidebar from "@/components/dashboard/sidebar"
+
+import {
+  useAppSelector,
+} from "@/lib/hook"
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const router =
+    useRouter()
+
+  const {
+    user,
+    loading,
+  } = useAppSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+
+    if (
+      !loading &&
+      !user
+    ) {
+      router.push("/login")
+    }
+
+  }, [
+    user,
+    loading,
+    router,
+  ])
+
+  if (loading) {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        Loading...
+      </div>
+    )
+  }
+
+  if (!user) {
+    return null
+  }
+
   return (
-    <div className="flex min-h-screen bg-zinc-100">
+    <div className="flex min-h-screen bg-zinc-100 dark:bg-background">
 
       {/* Sidebar */}
       <Sidebar />
